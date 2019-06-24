35b98f2dfa0c2479713df8faacae4524df1162bf
Alan Burgoyne <alan.burgoyne@frogasia.com>
2019-06-24 21:31:22 +0800

    refactor(test): entrypoint to ts

--- webpack.test.js
+++ webpack.test.js
@@ -3,7 +3,7 @@ const config = require('./webpack.config.js');
 module.exports = {
   ...config,
   mode: 'development',
-  entry: './test/index.js',
+  entry: './test/index.ts',
   output: {
     path: path.join(__dirname, 'test'),
     filename: 'bundle.js'
