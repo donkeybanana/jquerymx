cd23b6908c12c00641ff65e0b929b5d8fb6ef274
Alan Burgoyne <alan.burgoyne@frogasia.com>
2019-06-24 14:57:13 +0800

    refactor(deparam): remove from jquery

--- test/lang/string/deparam_test.js
+++ test/lang/string/deparam_test.js
@@ -1,45 +1,45 @@
-import '../../../lang/string/deparam/deparam.js';
+import deparam from '../../../lang/string/deparam/deparam.js';
 import { assert, module, test } from 'qunit/qunit/qunit.js';
 
 module('lang/string/deparam');
 
 test('Basic deparam', function() {
-  var data = $.String.deparam('a=b');
+  var data = deparam('a=b');
   assert.equal(data.a, 'b');
 
-  var data = $.String.deparam('a=b&c=d');
+  var data = deparam('a=b&c=d');
   assert.equal(data.a, 'b');
   assert.equal(data.c, 'd');
 });
 test('Nested deparam', function() {
-  var data = $.String.deparam('a[b]=1&a[c]=2');
+  var data = deparam('a[b]=1&a[c]=2');
   assert.equal(data.a.b, 1);
   assert.equal(data.a.c, 2);
 
-  var data = $.String.deparam('a[]=1&a[]=2');
+  var data = deparam('a[]=1&a[]=2');
   assert.equal(data.a[0], 1);
   assert.equal(data.a[1], 2);
 
-  var data = $.String.deparam('a[b][]=1&a[b][]=2');
+  var data = deparam('a[b][]=1&a[b][]=2');
   assert.equal(data.a.b[0], 1);
   assert.equal(data.a.b[1], 2);
 
-  var data = $.String.deparam('a[0]=1&a[1]=2');
+  var data = deparam('a[0]=1&a[1]=2');
   assert.equal(data.a[0], 1);
   assert.equal(data.a[1], 2);
 });
 
 test('deparam an array', function() {
-  var data = $.String.deparam('a[0]=1&a[1]=2');
+  var data = deparam('a[0]=1&a[1]=2');
 
-  assert.ok($.isArray(data.a), 'is array');
+  assert.ok(Array.isArray(data.a), 'is array');
 
   assert.equal(data.a[0], 1);
   assert.equal(data.a[1], 2);
 });
 
 test('deparam object with spaces', function() {
-  var data = $.String.deparam('a+b=c+d&+e+f+=+j+h+');
+  var data = deparam('a+b=c+d&+e+f+=+j+h+');
 
   assert.equal(data['a b'], 'c d');
   assert.equal(data[' e f '], ' j h ');
